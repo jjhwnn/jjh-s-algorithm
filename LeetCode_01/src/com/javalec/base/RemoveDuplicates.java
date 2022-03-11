5c36ff0f7721623cb960eb7c8a658155e574b4d0
jjhwnn <jeahwan174@gmail.com>
2022-03-11 17:51:31 +0900

    update LeetCode

--- LeetCode_01/src/com/javalec/base/RemoveDuplicates.java
+++ LeetCode_01/src/com/javalec/base/RemoveDuplicates.java
@@ -8,7 +8,7 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
 		RemoveDuplicates duplicates = new RemoveDuplicates();
 		
-		int[] nums = {1,1,2};
+		int[] nums = {98,99,99,100};
 		
 		int result = duplicates.removeDuplicates(nums);
 		
@@ -20,33 +20,38 @@ public class RemoveDuplicates {
 		// 각 고유 요소가 한 번만 나타나도록 합니다.
 		// 원소의 상대적 순서는 동일하게 유지되어야 합니다.
 		
-		int result = 0;
+		int count = 0;
 		
 		for(int i = 0; i < nums.length; i++) {
 			for(int j = i+1; j < nums.length; j++) {
 				
-				if(nums[i] == nums[j]) {
-					nums[j] = '_';
-					int tmp = nums[j];
-					nums[j] = nums[nums.length-1];
-					nums[nums.length-1] = nums[j];
+				if(j <= nums.length-1 && nums[i] == nums[j]) {
 					
-					result += 1;
+					nums[j] = '_';
+					
 				}
 				
-				
-				
 			}
+			
+		}
+
+		for(int i = 0; i < nums.length; i++) {
+			
 			if(nums[i] == '_') {
-				nums[i] = nums[nums.length-1];
+				count += 1;
 			}
+			
+		}
+		
+		Arrays.sort(nums);
+		
+		for(int i = 0; i < nums.length; i++) {
+			
+			System.out.print(nums[i] + " ");
 		}
 		
 		
-//		Arrays.sort(nums);
-
-
 		
-		return result;
+		return nums.length - count;
     }
 }
